ab2fb37b2b069e450491487ac613ef9f0bbb1821
MohamedTab <m.tabib@live.fr>
2014-11-11 17:08:58 +0100

    task manager without status

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,6 +14,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
+    redirect_to tasks_path
   end
 
   def edit
@@ -23,9 +24,13 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
     @task.update(task_params)
+    redirect_to tasks_path
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
